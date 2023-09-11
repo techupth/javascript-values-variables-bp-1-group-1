b6e00a02f17042cc6755ee815e410cb8b532d706
gritsakan <gritsakan1133@gmail.con>
2023-09-11 11:08:05 +0700

    feat:add new assignt(nine)

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,17 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let name = "Karter";
+let latsName = "Adderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantsName = "Chef's Table by Chef James";
+const netBill = 1500;
+const restaurantsOpen = 1500;
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+netBill = 5000;
+restaurantsOpen = 2500;

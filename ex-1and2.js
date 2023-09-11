567ff1e311d3010a4277f62b93d56f0a7443ea61
vuttipat <vuttipat.rungrojbribun@gmail.com>
2023-09-11 23:01:52 +0700

    assignment:dear

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,21 @@
-//Exercise #1: Variable Creation
+// Exercise #1: Variable Creation 
+//Start coding here 
+let name = "Karter"
+let lastname = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+
+const Restaurant_name = "Chef's Table by Chef James"
+const totalPrice = 15000
+const totalNumberofrestaurants = 15000
+
+// Exercise #2: Variable Reassignment
 //Start coding here
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641"
+totalPrice = 5000
+totalNumberofrestaurants = 2500
+
+
+
+
+
